a88195080b5c4e6a389e9e6e02f7c705a6fe5001
karads <85748703+karads@users.noreply.github.com>
2025-01-20 02:29:15 +0300

    kur.sh

---
+++ kur.sh
@@ -0,0 +1,29 @@
+sudo apt update -y
+apt-get update -y
+sudo apt install git python3-pip make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
+sudo apt update -y && sudo apt upgrade -y && sudo apt install -y screen rand unzip nano curl libsodium-dev cmake g++ git build-essential libgmp-dev libnuma-dev net-tools bc python3 python3-pip dos2unix
+sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo apt update -y
+sudo apt install python3.10 -y
+apt install unzip -y
+apt install unzip nano screen ifstat rand fuse3 nload -y
+sudo -v ; curl https://rclone.org/install.sh | sudo bash
+sudo apt update -y
+mv rclone.log /root/
+mkdir /mnt/{temp,pw,up1,up2,up3,up4,up5,up6,up7,up8,up9,up10}
+mkdir /root/.config/
+mkdir /root/.config/rclone/
+mv rclone.conf /root/.config/rclone/
+mv accounts.zip /root/
+mdkir /root/check1/
+mdkir /root/check2/
+mdkir /root/check3/
+mdkir /root/check4/
+mdkir /root/check5/
+mdkir /root/check6/
+mdkir /root/check7/
+mdkir /root/check8/
+mdkir /root/check9/
+mdkir /root/check10/
+unzip /root/accounts.zip
+screen -dmS bas bash start.sh

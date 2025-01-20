d4390a3b68b800df5e34b3e498f757ebb75a1f2d
karads <85748703+karads@users.noreply.github.com>
2025-01-20 04:22:12 +0300

    kur.sh

--- kur.sh
+++ kur.sh
@@ -2,6 +2,7 @@ sudo apt update -y
 apt-get update -y
 sudo apt install git python3-pip make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
 sudo apt update -y && sudo apt upgrade -y && sudo apt install -y screen rand unzip nano curl libsodium-dev cmake g++ git build-essential libgmp-dev libnuma-dev net-tools bc python3 python3-pip dos2unix
+sudo apt-get install inotify-tools -y
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt update -y
 sudo apt install python3.10 -y
@@ -11,11 +12,11 @@ sudo -v ; curl https://rclone.org/install.sh | sudo bash
 sudo apt update -y
 mv rclone.log /root/
 mkdir /mnt/{temp,pw,up1,up2,up3,up4,up5,up6,up7,up8,up9,up10}
+for i in {1..10}; do mkdir -p /root/check$i; done
 mkdir /root/.config/
 mkdir /root/.config/rclone/
 mv rclone.conf /root/.config/rclone/
 mv accounts.zip /root/
-for i in {1..10}; do mkdir -p /root/check$i; done
 unzip /root/accounts.zip
 chmod 777 client
 screen -dmS bas bash start.sh

c15f51641dae64deb3723c7e5cb6d2472f40a92b
karads <85748703+karads@users.noreply.github.com>
2025-01-20 04:29:30 +0300

    move.sh

--- move.sh
+++ move.sh
@@ -19,10 +19,23 @@ index=0
 echo "Starting..."
 
 while true; do
+  # Wait for new .fpt files in the source directory
   file=$(inotifywait -q -e create,moved_to --format '%w%f' /mnt/pw/)
+  
   if [[ $file == *.fpt ]]; then
-    mv "$file" "${destinations[$index]}"
-    echo "Moved $file to ${destinations[$index]}"
+    # Get the destination directory
+    destination="${destinations[$index]}"
+    
+    # Move the file with a temporary name
+    temp_file="${destination}$(basename "$file").tmp"
+    mv "$file" "$temp_file"
+    
+    # Rename the file to its final name once fully moved
+    final_file="${destination}$(basename "$file")"
+    mv "$temp_file" "$final_file"
+    
+    echo "Moved $file to $final_file"
+    
     # Increment index or reset to 0 if we've hit the end of the destinations array
     ((index=(index+1)%${#destinations[@]}))
   fi
